6047cc57306cd035e100d0d0988c4829ee3b9a6a
JohnPaulCadorniga <jpcadorniga12@gmail.com>
2025-10-21 21:56:50 +0800

    added routing and minor adjusments

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def login():
     return render_template('login.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/welcomepage')
+def welcomepg():
+    return render_template('welcomepg.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
